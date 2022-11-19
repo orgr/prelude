92ca6bb806348e7bd69ff3c224a2c64315f88e09
Or Grinberg <or.grin@gmail.com>
2022-11-19 07:13:56 +0200

    Add M-o to switch windows and M-n/M-p to cycle through symbol occurrences

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -1,2 +1,10 @@
 (global-set-key (kbd "M-/") 'xref-find-references)
 (global-set-key (kbd "C-`") 'easy-mark)
+(define-key prelude-mode-map (kbd "M-o") 'ace-window)
+
+;; use symbol overlay for navigating to next symbol occurance with M-n/M-p
+(prelude-require-package 'symbol-overlay)
+(global-set-key (kbd "M-n") 'symbol-overlay-jump-next)
+(global-set-key (kbd "M-p") 'symbol-overlay-jump-prev)
+(setq symbol-overlay-idle-time 0.1)
+(add-hook 'prog-mode-hook (lambda () (symbol-overlay-mode 1)))
