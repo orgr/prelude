2f36d888b141ba0f38ee5e8faf026e0dddb84036
Or Grinberg <or.grin@gmail.com>
2022-12-16 14:49:02 +0000

    Add multiple cursor (with swiper), snap buffer after split or help window, add my custom.el with less-ugly tabs

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -1,6 +1,5 @@
 (global-set-key (kbd "M-/") 'xref-find-references)
 (define-key lsp-ui-mode-map (kbd "M-/") 'lsp-ui-peek-find-references)
-(global-set-key (kbd "C-`") 'easy-mark)
 (define-key prelude-mode-map (kbd "M-o") 'ace-window)
 (global-set-key (kbd "C-M-;") 'hippie-expand)
 
@@ -12,4 +11,6 @@
 (add-hook 'prog-mode-hook (lambda () (symbol-overlay-mode 1)))
 
 ;;switch to last buffer
-(global-set-key (kbd "C-c b") 'crux-switch-to-previous-buffer)
+(global-set-key (kbd "C-c c") 'crux-switch-to-previous-buffer)
+
+(prelude-require-package 'multiple-cursors)
--- personal/ui.el
+++ personal/ui.el
@@ -4,3 +4,6 @@
 
 (setq lsp-ui-peek-always-show nil)
 (set-scroll-bar-mode nil)
+(setq help-window-select t)
+(global-set-key "\C-x2" (lambda () (interactive)(split-window-vertically) (other-window 1)))
+(global-set-key "\C-x3" (lambda () (interactive)(split-window-horizontally) (other-window 1)))
